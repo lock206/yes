fda6cc697c313287a874393e8062f4dc4941d261
Jonathan I. Lockwood <jilockwo@csis.svsu.edu>
2018-02-25 12:59:57 -0500

    Updated comitt

--- test.java
+++ test.java
@@ -1,1 +1,27 @@
-hello world!
+
+import java.util.Scanner;
+
+public class prog4 {
+  public static void main (String[] args) {
+    Scanner reader = new Scanner(System.in);
+
+    //declare variables
+    String name = "Name";
+    String scr = "Score";
+    String StNu = "Student #";
+
+    double score;
+    double minScore;
+    double maxScore;
+    double avgScore;
+    int studNum;
+    int numRecords;
+    int count = 0;
+
+    //data entry & calculations
+
+    for(int i = 0; reader.next()!= null; i++);
+ }
+}
+
+
